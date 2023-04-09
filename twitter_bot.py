6c09b570d50ccc65b2c41968ef80310b79ca6c8d
Ehsan U <lancerf562@gmail.com>
2023-04-09 10:30:03 +0500

    add coda changes

--- twitter_bot.py
+++ twitter_bot.py
@@ -3,43 +3,50 @@ from scrapy.crawler import CrawlerProcess
 from scrapy import signals
 import tweepy
 import datetime
-
+import re
 
 
 class Twitter(scrapy.Spider):
     name = "twitter_spider"
+    email_regx = re.compile('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}', re.IGNORECASE)
 
 
     def start_requests(self):
         for keyword in self.keywords:
-            yield scrapy.Request(url='https://twitter.com', callback=self.parse, cb_kwargs={'query': keyword['keyword'], "min_followers": keyword['min_followers'], 'maxResults':keyword['maxResults']})
+            yield scrapy.Request(url='https://twitter.com', callback=self.parse, cb_kwargs=keyword)
 
 
-    def parse(self, response, query, min_followers, maxResults):
-        tweets = self.api.search_tweets(query, count=maxResults, tweet_mode='extended', result_type='recent', lang='en')
+    def parse(self, response, keyword, iDOutRequest,  min_followers, maxResults):
+        tweets = self.api.search_tweets(keyword, count=maxResults, tweet_mode='extended', result_type='recent', lang='en')
         for tweet in tweets:
             if 'RT @' not in tweet.full_text:
                 followers = tweet.user.followers_count
                 if followers < min_followers:
                     continue
-                created_at = tweet.created_at
-                username = tweet.user.screen_name    
-                tweet_link = f'https://twitter.com/{username}/status/' + tweet.id_str
-                profile = 'https://twitter.com/' + username
+                channelName = tweet.user.screen_name    
+                recent_tweets = self.get_user_tweets(self.api, channelName)
+                pinned_tweet = recent_tweets[0]._json['full_text'] if len(recent_tweets) > 0 else None
+                channelURL = 'https://twitter.com/' + channelName
                 item = {
-                    'timestamp': str(created_at.time()),
-                    'datestamp': str(created_at.date()),
-                    'username': username,
-                    'user_location': tweet.user.location,
-                    'profile_description': tweet.user.description,
-                    'tweet_link': tweet_link,
-                    'profile': profile,
-                    'user_id': tweet.user.id,
-                    'tweet_text': tweet.full_text,
-                    'followers': tweet.user.followers_count,
-                    'retweets': tweet.retweet_count,
+                    "keyword": keyword,
+                    "iDOutRequest": iDOutRequest,
+                    "channelId": tweet.user.id_str,
+                    'channelName': channelName,
+                    'location': tweet.user.location,
+                    'channelDescription': tweet.user.description,
+                    'last_50_tweets': r"{}".join([tweet._json['full_text'] for tweet in recent_tweets]),
+                    'pinned_tweet': pinned_tweet,
+                    'channelURL': channelURL,
+                    'metric_Subscribers': tweet.user.followers_count,
+                    "emailfromChannelDescription": ",".join(self.email_regx.findall(tweet.user.description)),
                 }
                 yield item
+
+
+    @staticmethod
+    def get_user_tweets(api, screen_name, count=50):
+        tweets = api.user_timeline(screen_name=screen_name, count=count, tweet_mode='extended')
+        return tweets
 
 
     @classmethod
@@ -60,5 +67,5 @@ crawler = CrawlerProcess(settings={
     "HTTPCACHE_ENABLED": True,
     "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
 })
-crawler.crawl(Twitter, keywords=[{"keyword": "python", "min_followers": 100, "maxResults":100}])
+crawler.crawl(Twitter, keywords=[{"keyword": "realpython", "iDOutRequest":1, "min_followers": 100, "maxResults":100}])
 crawler.start()
